cee4c53cbe52ebbf0013418322339cae5ce70f4a
Aniruddha-Shukla <aniruddhashukla2005@gmail.com>
2026-08-02 20:33:58 +0530

    use secure cookies with SameSite=None for cross-origin

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -59,7 +59,7 @@ export const logOut = async (req, res) => {
         const sessionId = req.cookies?.session
         await redis.del(`session-${sessionId}`)
 
-        res.clearCookie("session")
+        res.clearCookie("session", { httpOnly: true, secure: true, sameSite: "none" })
         return res.status(200).json({ message: "logout successfully" })
     } catch (error) {
         return res.status(500).json({ message: `logout error ${error}` })
